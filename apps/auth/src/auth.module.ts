a9eac34a6b694bfe52ab9cdff9a99810cea023ff
Bertoldo Klinger <bertoldokbrj@gmail.com>
2024-03-14 10:24:55 -0300

    feat: add authentication login endpoint and implement password hashing

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -6,6 +6,7 @@ import { LoggerModule } from '@app/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [UsersModule, LoggerModule,ConfigModule.forRoot({
@@ -27,6 +28,6 @@ import * as Joi from 'joi';
   })],
   
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs'
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersRepository } from './users.repository';
 
@@ -7,6 +8,19 @@ export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
   async create(createUserDto: CreateUserDto) {
-    return this.usersRepository.create(createUserDto);
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10)
+
+    return this.usersRepository.create({...createUserDto, password: hashedPassword });
+  }
+
+  async verifyUser(email: string, password: string) {
+    const user = await this.usersRepository.findOne({ email })
+    const isValidPassword = await bcrypt.compare(password, user.password)
+
+    if(!isValidPassword) {
+      throw new UnauthorizedException('Credentials are not valid!')
+    }
+
+    return user
   }
 }
