9f09546a5ea189166932be3f42c1598a0e7c9318
Bertoldo Klinger <bertoldokbrj@gmail.com>
2024-03-22 17:12:04 -0300

    build: add service deployments on microservices

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: () => ({
-        uri: 'mongodb://mongo:27017/sleepr',
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
       }),
       inject: [ConfigService],
     }),
