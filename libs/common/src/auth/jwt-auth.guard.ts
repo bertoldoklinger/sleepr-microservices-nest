be2045c571c10f57dd3efc4cacc5a30e87a554b6
Bertoldo Klinger <bertoldokbrj@gmail.com>
2024-03-26 15:17:03 -0300

    refactor: refactor authentication service and strategy

--- libs/common/src/auth/jwt-auth.guard.ts
+++ libs/common/src/auth/jwt-auth.guard.ts
@@ -11,7 +11,8 @@ export class JwtAuthGuard implements CanActivate {
   constructor(@Inject(AUTH_SERVICE) private readonly authClient: ClientProxy){}
 
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-    const jwt = context.switchToHttp().getRequest().cookies?.Authentication || context.switchToHttp().getRequest().headers?.authentication
+    const jwt = context.switchToHttp().getRequest().cookies?.Authentication ||
+      context.switchToHttp().getRequest().headers?.authentication;
     if(!jwt) {
       return false
     }
